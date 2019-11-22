c39f596591015d8f44cd2db0c680677ebc37f50b
deedee <mymusese@gmail.com>
2019-11-22 18:58:52 +0900

    triggerCallback을 string으로 설정
    - function으로 선언하고 string으로 바꾸면, 프레임워크에 따라 앞에 function이 붙는 경우가 생길 수 있어 웹뷰가 javascript 실행할때 오류 발생
    - 때문에 관리는 어렵지만 string으로 선언해 넘기도록 로직 변경

--- src/plugin.ts
+++ src/plugin.ts
@@ -12,38 +12,42 @@ const { IamportCapacitor, Device } = Plugins;
 const REDIRECT_URL = 'http://localhost/iamport';
 
 export class IMP implements IamportCapacitorPlugin {
+  private isCallbackCalled: boolean = false;       
+  private triggerCallback: string = `function(response) {
+      const query = [];
+      Object.keys(response).forEach(key => {
+        query.push(key + '=' + response[key]);
+      });
+
+      location.href = 'http://localhost/iamport?' + query.join('&');
+    }`;         
+
   addListener(callback: any, type?: String) {
     IamportCapacitor.addListener('IMPOver', async ({ url }: any) => {
 
-      const { platform } = await Device.getInfo();
-      if (platform === 'ios' && type === 'inicis') {
-        /** 
-         * IOS && 이니시스 && 실시간 계좌이체 예외처리
-         * 리디렉션 URL: io.ionic.starter://?imp_uid=imp_048747651691%26merchant_uid=mid_1573712730635%26m_redirect_url=http%3A%2F%2Flocalhost%2Fiamport%3Fimp_uid%3Dimp_048747651691%26merchant_uid%3Dmid_15737127306352
-        */
-        const decodedUrl = decodeURIComponent(url);
-        const extractedQuery = queryString.extract(decodedUrl);
-        const parsedQuery = queryString.parse(extractedQuery);
-        const { imp_uid, merchant_uid } = parsedQuery;
-        const query = {
-          imp_uid,
-          merchant_uid: typeof merchant_uid === 'object' ? merchant_uid[0] : merchant_uid,
-        };
-        callback(query);
-      } else {
-        const { query } = queryString.parseUrl(url);
-        callback(query);
+      if (!this.isCallbackCalled) { // 콜백 중복 호출 방지
+        const { platform } = await Device.getInfo();
+        if (platform === 'ios' && type === 'inicis') {
+          /** 
+           * IOS && 이니시스 && 실시간 계좌이체 예외처리
+           * 리디렉션 URL: io.ionic.starter://?imp_uid=imp_048747651691%26merchant_uid=mid_1573712730635%26m_redirect_url=http%3A%2F%2Flocalhost%2Fiamport%3Fimp_uid%3Dimp_048747651691%26merchant_uid%3Dmid_15737127306352
+          */
+          const decodedUrl = decodeURIComponent(url);
+          const extractedQuery = queryString.extract(decodedUrl);
+          const parsedQuery = queryString.parse(extractedQuery);
+          const { imp_uid, merchant_uid } = parsedQuery;
+          const query = {
+            imp_uid,
+            merchant_uid: typeof merchant_uid === 'object' ? merchant_uid[0] : merchant_uid,
+          };
+          callback(query);
+        } else {
+          const { query } = queryString.parseUrl(url);
+          callback(query);
+        }
+        this.isCallbackCalled = true;
       }
     });
-  }
-
-  triggerCallback(response: any) { // 콜백을 지원하는 PG사의 경우, 결제 시도 종료 후 해당 함수가 트리거된다
-    const query: any = [];
-    Object.keys(response).forEach(key => {
-      query.push(`${key}=${response[key]}`);
-    });
-
-    location.href = `http://localhost/iamport?${query.join('&')}`;
   }
 
   payment(options: PaymentOptions): Promise<PaymentOptions> {
@@ -56,7 +60,7 @@ export class IMP implements IamportCapacitorPlugin {
         ...data,
         m_redirect_url: REDIRECT_URL,
       },
-      triggerCallback: String(this.triggerCallback),
+      triggerCallback: this.triggerCallback,
       redirectUrl: REDIRECT_URL,
     };
     this.addListener(callback, type);
@@ -83,7 +87,7 @@ export class IMP implements IamportCapacitorPlugin {
       type: 'certification',
       userCode,
       data,
-      triggerCallback: String(this.triggerCallback),
+      triggerCallback: this.triggerCallback,
       redirectUrl: REDIRECT_URL,
     };
     this.addListener(callback);
