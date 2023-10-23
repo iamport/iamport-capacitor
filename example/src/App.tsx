7808ba14acec4030c610e54faeee037df44c7c75
Hyukgyu Kwon <me@kwon.hk>
2023-10-23 14:54:14 +0900

    feat: upgrade example capacitor version to v5

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import {IonApp, IonRouterOutlet, setupIonicReact} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import Payment from './pages/Payment';
@@ -25,6 +25,8 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
